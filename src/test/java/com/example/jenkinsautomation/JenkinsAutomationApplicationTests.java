0aacb994de0cd2c0438e84c8c4177c849dbc61ac
seoyunch <yen731013@naver.com>
2025-10-30 12:38:57 +0900

    commit

---
+++ src/test/java/com/example/jenkinsautomation/JenkinsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
